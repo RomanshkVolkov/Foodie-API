e51615c5f538ea06e067c8b0d7e9a0f92613aaaf
Jose Guzman <joseguzmandev@gmail.com>
2023-04-07 19:43:46 -0500

    Prepare to vercel deployment

--- api_foodie/settings.py
+++ api_foodie/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m-nz2f@5wmo2b2+ts6dx$a#^ncjtyxnb@3$u03e#u2z=-kpnt!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -132,3 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
