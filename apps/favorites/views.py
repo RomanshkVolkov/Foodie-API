f2e71f23615520fc336002c3898fc305038ac3d1
langelX <langelxobl@gmail.com>
2023-04-19 10:18:02 -0500

    fix delete favorite

--- apps/favorites/views.py
+++ apps/favorites/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from apps.recipe.models.recipe import Favorite
@@ -23,3 +24,24 @@ class FavoritesView(viewsets.ModelViewSet):
         if (self.action == 'create'):
             return FavoriteSerializer
         return GetFavoriteSerializer
+
+    def destroy(self, request, pk=None):
+        favorite = self.get_object()
+        favorite.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def get_object(self):
+        # favorite = get_object_or_404(Favorite, id=self.kwargs.get('pk'))
+        try:
+            favorite = Favorite.objects.get(id=self.kwargs.get('pk'))
+        except Favorite.DoesNotExist:
+
+            # Si no se encontró, se busca por usuario y receta
+            # if not favorite:
+            print("entro a ")
+            user = self.request.user
+            recipe_id = self.kwargs.get('pk')
+            favorite = get_object_or_404(
+                Favorite, user=user, recipe_id=recipe_id)
+
+        return favorite
